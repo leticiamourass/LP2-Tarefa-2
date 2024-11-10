4438907730ddeab010af32a30a55ba9c73ae36d1
Letícia Moura <141355668+leticiamourass@users.noreply.github.com>
2024-11-10 20:52:18 -0300

    ClasseMain.java

---
+++ Superclasse abstrata Animal/ClasseMain.java
@@ -0,0 +1,15 @@
+// Classe Main para testar as classes Animal, Dog e Cat
+public class Main {
+   public static void main(String[] args) {
+      // Criando objetos Dog e Cat
+      Animal myDog = new Dog();
+      Animal myCat = new Cat();
+      
+      // Chamando os métodos makeSound() e sleep() em ambos os objetos
+      myDog.makeSound();  // Saída: Woof! Woof!
+      myDog.sleep();      // Saída: This animal is sleeping.
+      
+      myCat.makeSound();  // Saída: Meow! Meow!
+      myCat.sleep();      // Saída: This animal is sleeping.
+   }
+}
