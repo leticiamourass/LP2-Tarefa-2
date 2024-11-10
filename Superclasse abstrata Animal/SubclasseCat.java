4a53cbd55f43580bd8625940ced51b76e2f7c920
Letícia Moura <141355668+leticiamourass@users.noreply.github.com>
2024-11-10 20:50:58 -0300

    SubclasseCat.java

---
+++ Superclasse abstrata Animal/SubclasseCat.java
@@ -0,0 +1,9 @@
+// Subclasse Cat
+public class Cat extends Animal {
+   
+   // Implementando o método makeSound() que foi declarado na classe Animal
+   @Override
+   public void makeSound() {
+      System.out.println("Meow! Meow!");
+   }
+}
